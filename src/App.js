46348dc85c150be51c30a2dbb0159b34df6ffe92
Suras Kariyil <suraskariyil@gmail.com>
2024-01-04 21:48:53 +0530

    logo updated

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import "./App.css"
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Banner/>
+      <RowPost/>
+
+    </div>
+   
+  );
+}
+
+export default App;
